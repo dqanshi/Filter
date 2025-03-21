8763761a95674d5c46b7f0460adce2759cc345cc
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2025-03-21 14:58:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,6 +2,8 @@ from asyncio.log import logger
 import re
 from os import environ
 
+from decouple import config
+
 id_pattern = re.compile(r'^.\d+$')
 def is_enabled(value, default):
     if value.lower() in ["true", "yes", "1", "enable", "y"]:
